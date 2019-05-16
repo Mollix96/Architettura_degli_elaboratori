4fbad51f96aecee21ec62fe2a8f3cd42a8655b45
Mirco Molinari <mollix96@gmail.com>
2019-05-16 13:56:28 +0200

    aggiornati nomi

--- MIPS/010_Maggiore/maggiore.asm
+++ MIPS/010_Maggiore/maggiore.asm
@@ -21,11 +21,11 @@ main:
     syscall                         # Lettura del valore.
     move  $s1, $v0                  # Salva il valore letto.
 
-                                    # Chiama la funzione 'isGreaterThan'.
+                                    # Chiama la funzione 'setGreaterThan'.
     addi  $sp, $sp, -8              # Decrementa lo stack pointer di 8 byte (spazio per 2 valori).
     sw    $s0, 0($sp)               # Carica il primo valore sullo stack.
     sw    $s1, 4($sp)               # Carica il secondo valore sullo stack.
-    jal   isGreaterThan             # Chiama 'isGreaterThan'.
+    jal   setGreaterThan            # Chiama 'setGreaterThan'.
 
     move  $t0, $v0                  # Salva il valore di ritorno.
 
@@ -61,7 +61,7 @@ exit:
 # $v0 = 0x0  Se il primo valore e' minore del secondo.
 # $v0 = 0x1  Se il primo valore e' maggiore del secondo.
 #
-isGreaterThan:
+setGreaterThan:
                                     # Pop dei valori dallo stack.
     lw    $t0, 0($sp)               # Pop del primo valore.
     lw    $t1, 4($sp)               # Pop del secondo valore.
@@ -69,13 +69,13 @@ isGreaterThan:
 
     li    $v0, 0x0                  # Inizializza il valore di ritorno a false.
 
-    beq   $t0, $t1, igt_return      # Se i due falori sono uguali, restituisce false.
+    beq   $t0, $t1, sgt_return      # Se i due falori sono uguali, restituisce false.
 
     slt   $t2, $t0, $t1             # Se il primo valore e' il minore, imposta ($t2 = 1)...
-    beq   $t2, 0x1, igt_return      # ...e restituisce false.
+    beq   $t2, 0x1, sgt_return      # ...e restituisce false.
 
                                     # Se si arriva qui, il primo valore e' maggiore del secondo.
     li    $v0, 0x1                  # Imposta il valore di ritorno a 0x1.
-igt_return:
+sgt_return:
     jr    $ra                       # Torna al chiamante.
 #endregion
